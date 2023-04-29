8377ee37284fd3295a72f4a1ebdff8f9f4b86898
Savio Dias Viana <saviodiasviana@gmail.com>
2023-04-29 11:18:05 -0300

    Get, Put e Delete do carrinho

--- src/Schemas/cart.schema.js
+++ src/Schemas/cart.schema.js
@@ -5,3 +5,8 @@ export const cartSchema = Joi.object({
     color: Joi.string().pattern(/^#([0-9A-Fa-f]{3}){1,2}$/).required(),
     number: Joi.number().integer().min(1).required()
 })
+
+export const updateCartSchema = Joi.object({
+    id: Joi.string().required(),
+    amount: Joi.number().integer().min(1).required()
+})
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { createCartList } from "../controllers/cart.controller.js";
+import { createCartList, deleteCartList, getCartList, updateCartList } from "../controllers/cart.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
-import { cartSchema } from "../Schemas/cart.schema.js";
+import { cartSchema, updateCartSchema } from "../Schemas/cart.schema.js";
 import { authValidation } from "../middlewares/authValidation.middlewares.js";
 
 const cartRouter = Router()
 
 cartRouter.use(authValidation)
-cartRouter.post("/cart/:id",validateSchema(cartSchema), createCartList)
+cartRouter.post("/cart/:id", validateSchema(cartSchema), createCartList)
+cartRouter.get("/cart", getCartList)
+cartRouter.put("/cart", validateSchema(updateCartSchema), updateCartList)
+cartRouter.delete("/cart", deleteCartList)
 
 export default cartRouter;
