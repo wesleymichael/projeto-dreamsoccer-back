c201a04d80a611f7b470858a54ce4f876faaa803
Savio Dias Viana <saviodiasviana@gmail.com>
2023-04-29 13:40:38 -0300

    Alteração na requisição do filtro

--- src/middlewares/filterPlayers.middeware.js
+++ src/middlewares/filterPlayers.middeware.js
@@ -1,45 +1,62 @@
-export default function filterPlayer(req, res, next){
-    const { category, position, type, nacionality } = req.query;
+export default function filterPlayer(req, res, next) {
+    const { category, position, type, nationality } = req.query;
     const page = req.query.page ? parseInt(req.query.page) : 1;
     const validPosition = ["ATA", "M", "D", "G"];
     const validCategory = ["M", "F"];
     const validType = ["Legendary", "Silver", "Bronze"];
-
+    const validNationality = ["BRA", "Internacional"]
     const filter = {};
     const errors = [];
 
-    if(page < 1 || isNaN(page)){
+    if (page < 1 || isNaN(page)) {
         errors.push("Página informada inválida!");
     }
 
-    if (category){
-        if ( validCategory.includes(category) ){
+    if (category) {
+        if (validCategory.includes(category)) {
             filter.category = category;
         } else {
             errors.push("Variável \"category\" incorreta!");
-        }   
+        }
     }
 
-    if (position){
-        if ( validPosition.includes(position) ){
-            filter.position = position;
-        } else {
-            errors.push("Variável \"position\" incorreta!");
-        }   
+    if (position) {
+        if (typeof position === "string") {
+            if (validPosition.includes(position)) {
+                filter.position = position;
+            } else {
+                errors.push("Variável \"position\" incorreta!");
+            }
+        } else{
+            const array =[]
+            position.forEach((element, i) => {
+                if (validPosition.includes(element)) {
+                    array.push(element);
+                } else {
+                    errors.push("Variável \"position\" incorreta!");
+                }
+            });
+            filter.position = array
+        }
     }
 
-    if (type){
-        if ( validType.includes(type) ){
+    if (type) {
+        if (validType.includes(type)) {
             filter.type = type;
         } else {
             errors.push("Variável \"type\" incorreta!");
-        }   
+        }
+    }
+    if (nationality) {
+        if (validNationality.includes(nationality)) {
+            filter.nationality = nationality;
+        } else {
+            errors.push("Variável \"nationality\" incorreta!");
+        }
     }
 
-    if (nacionality) filter.nacionality = nacionality;
-
-    if(errors.length !== 0){
-        return res.status(422).send(errors); 
+    if (errors.length !== 0) {
+        return res.status(422).send(errors);
     }
 
     res.locals.page = page;
