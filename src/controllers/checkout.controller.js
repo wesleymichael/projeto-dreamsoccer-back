1034580e4508805ab59c6c9c3c3d65f8e024d101
taisoliva <tais15oliva@gmail.com>
2023-05-02 14:06:39 -0300

    Feat: Funcionalidade de Email

--- src/controllers/checkout.controller.js
+++ src/controllers/checkout.controller.js
@@ -1,4 +1,5 @@
 import { db } from "../database/database.connection.js";
+import mail from "@sendgrid/mail"
 
 export async function checkout(req, res) {
     const acesso = res.locals.session;
@@ -42,5 +43,25 @@ export async function deleteCart (req, res){
         res.status(202).send("Compra finalizada")
     } catch (err) {
         res.status(500).send(err.message)
+    }
+}
+
+export async function sendEmail(req, res){
+    const {email} = req.body
+
+    try{
+        mail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            to: email,
+            from:"tais15oliva@gmail.com",
+            subject:"Confirmação de compra",
+            html:"<p> Olá, seu pedido foi concluído com sucesso </p>"
+        }
+
+        await mail.send(msg)
+        console.log("Email enviado")
+        res.status(200).send("Email enviado com sucesso")
+    } catch (err){
+        return res.status(500).send(err.message)
     }
 }
--- src/routes/checkout.router.js
+++ src/routes/checkout.router.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authValidation } from "../middlewares/authValidation.middlewares.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { checkoutSchema } from "../Schemas/checkout.schema.js";
-import { checkout, deleteCart, getCheckout } from "../controllers/checkout.controller.js";
+import { checkout, deleteCart, getCheckout, sendEmail } from "../controllers/checkout.controller.js";
 
 const checkoutRouter = Router();
 
@@ -11,5 +11,7 @@ checkoutRouter.use(authValidation);
 checkoutRouter.post("/checkout", validateSchema(checkoutSchema), checkout);
 checkoutRouter.get("/checkout", getCheckout);
 checkoutRouter.delete("/finalizar", deleteCart)
+checkoutRouter.post("/email", sendEmail)
+
 
 export default checkoutRouter;
