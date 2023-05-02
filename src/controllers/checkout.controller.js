d77a7d04159b0431242a913c07f29987659601e6
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-02 01:34:15 -0300

    fix: no checkout o body recebe apenas o endereço

--- src/controllers/checkout.controller.js
+++ src/controllers/checkout.controller.js
@@ -1,8 +1,24 @@
 import { db } from "../database/database.connection.js";
 
 export async function checkout(req, res) {
+    const acesso = res.locals.session;
+
     try { 
-        await db.collection("checkout").insertOne({...req.body});
+        const cart = await db.collection("cart").find({ idUsuario: acesso.idUsuario }).toArray();
+
+        const items = cart.map(item => ({
+            itemId: item._id,
+            amount: item.amount,
+            color: item.color,
+            number: item.number,
+        }));
+        
+        const checkout = {
+            idUsuario: acesso.idUsuario,
+            items,
+            address: req.body,
+        }
+        await db.collection("checkout").insertOne(checkout);
         res.status(201).send("Compra finalizada");
     } catch (error) {
         res.status(500).send(error.message);
