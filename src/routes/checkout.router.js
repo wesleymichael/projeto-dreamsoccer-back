be012e4a680b46aaf17e60bd1153eea150a2e800
Savio Dias Viana <saviodiasviana@gmail.com>
2023-05-02 02:28:47 -0300

    Correção na rota de finalizar

--- src/routes/checkout.router.js
+++ src/routes/checkout.router.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authValidation } from "../middlewares/authValidation.middlewares.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { checkoutSchema } from "../Schemas/checkout.schema.js";
-import { checkout, getCheckout } from "../controllers/checkout.controller.js";
+import { checkout, deleteCart, getCheckout } from "../controllers/checkout.controller.js";
 
 const checkoutRouter = Router();
 
@@ -10,5 +10,6 @@ checkoutRouter.use(authValidation);
 
 checkoutRouter.post("/checkout", validateSchema(checkoutSchema), checkout);
 checkoutRouter.get("/checkout", getCheckout);
+checkoutRouter.delete("/finalizar", deleteCart)
 
 export default checkoutRouter;
